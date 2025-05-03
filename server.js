9cf69ade3645c0414a9b969b07cafbb07ad3bbe4
yeab166 <yeabsirabelete@aastustudent.edu.et>
2025-05-03 13:37:13 +0300

    add .env

---
+++ server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const cors= require('cors');
+const dotenv= require('dotenv');
+
+dotenv.config();
+const authRouter= require('../LostFoundNode/src/routes/authRoutes');
+
+const app= express();
+app.use(cors());
+app.use(express.json());
+app.use('/api',authRouter);
+
+const PORT= process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+  console.log(`The surver run on port ${PORT}`);
+});
