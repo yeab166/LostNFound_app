f6fe253297428c900857efb6aaa27016f4f194d5
Yeabsira Belete <yeababa16@gmail.com>
2025-05-05 09:28:53 +0300

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,11 +1,9 @@
 const admin = require('../config/fireBase');
 
-exports. authenticate = async (req, res, next) => {
-  const token = req.headers.authorization?.split('Bearer ')[1];
 
-  if (!token){
-    return res.status(401).json('Unauthorized');
-  } 
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization?.split('Bearer ')[1];
+  if (!token) return res.status(401).json('Unauthorized');
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
