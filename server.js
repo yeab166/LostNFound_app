69efd423d2d877ff0b45e3f523d3349c51da55bc
Yeabsira Belete <yeababa16@gmail.com>
2025-05-05 09:56:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/api',authRoutes);
 
 
-const PORT= process.env.PORT || 8080;
+const PORT= process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
   console.log(`The server run on port ${PORT}`);
