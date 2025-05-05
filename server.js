b77debe4b53098bc85ca8f7ce6dff0d5a48363b7
Yeabsira Belete <yeababa16@gmail.com>
2025-05-05 07:21:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const cors= require('cors');
 const dotenv= require('dotenv');
 
 dotenv.config();
-const authRoutes = require('../src/routes/authRoutes');
-
+const authRouter= require('../LostNFound_app/src/routes/authRoutes');
 
 const app= express();
 app.use(cors());
 app.use(express.json());
-app.use('/api',authRoutes);
+app.use('/api',authRouter);
 
 const PORT= process.env.PORT || 8080;
 
